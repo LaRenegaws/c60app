cc57b9798cb44fab0e84924c9050220d87dcadae
Aditya Keerthi <pxlcoder@gmail.com>
2015-10-11 17:07:12 -0400

    Update create.js

--- webapp/js/create.js
+++ webapp/js/create.js
@@ -1,34 +1,34 @@
 $(document).ready(function() {
 
-    var databaseRef = new Firebase("https://c60app.firebaseio.com/");
+    	var databaseRef = new Firebase("https://c60app.firebaseio.com/");
+    	
+	$( "#createbtn" ).click(function() {
 
-    $( "#createbtn" ).click(function() {
+	    	var passwordText = $("#passwordinput").val();
+	
+	    	var usersRef = databaseRef.child("users");
+	
+	    	var pebbleID = $('#pidinput').val();
+	  	var nameText = $('#nameinput').val();
+	  	var emailText = $('#emailinput').val();
+	  	var phoneText = $('#phoneinput').val();
+	      	var usernameText = $('#userinput').val();
+	
+	  	usersRef.child(pebbleID).set({
+	  		name: nameText,
+	  		email: emailText,
+	  		phone: phoneText,
+	        	username: usernameText,
+	        	connections: "Connections"
+		});
+	
+	      	var accountsRef = databaseRef.child("accounts");
+	
+		accountsRef.child(usernameText).set({
+			password: passwordText,
+			pebbleid: pebbleID
+		});
 
-    	var passwordText = $("#passwordinput").val();
-
-    	var usersRef = databaseRef.child("users");
-
-    	var pebbleID = $('#pidinput').val();
-  		var nameText = $('#nameinput').val();
-  		var emailText = $('#emailinput').val();
-  		var phoneText = $('#phoneinput').val();
-      var usernameText = $('#userinput').val();
-
-  		usersRef.child(pebbleID).set({
-  			name: nameText,
-  			email: emailText,
-  			phone: phoneText,
-        username: usernameText,
-        connections: "Connections"
-			});
-
-      var accountsRef = databaseRef.child("accounts");
-
-      accountsRef.child(usernameText).set({
-        password: passwordText,
-        pebbleid: pebbleID
-      });
-
-      window.location.href = "index.html";
+      		window.location.href = "index.html";
 	});
 });
